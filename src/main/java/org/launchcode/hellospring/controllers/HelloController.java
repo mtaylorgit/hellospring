3e9fa90f7bfaccbd69923fdf1419739ffc7f9395
oggie <morkthealien@gmail.com>
2023-10-01 12:54:08 -0500

    updating for next exercise

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,52 +1,83 @@
+//package org.launchcode.hellospring.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.*;
+//
+//@Controller
+//@ResponseBody//You can put this up here and delete all the others below
+//@RequestMapping("hello")//you can also put this up here - and it affects the route below, so
+//public class HelloController {
+//    //Handles request at path localhost:8080/hello
+////    @GetMapping("hello")//NOTE we use @PostMapping to do post requests
+////    @ResponseBody
+////    public String hello() {
+////        return "Hello, Spring!";
+////    }
+//    //this handles request at /goodbye returns a static response
+//    @GetMapping("goodbye")//putting this above at the top, you can just see that it lives at /hello/goodbye
+//    @ResponseBody
+//    public String goodbye() {
+//        return "Goodbye, Spring!";
+//    }
+//
+//    //create a handler that handles requests of /hello?name=LaunchCode you have to type localhost:8080/hello?name=name
+////    @GetMapping("hello")//dynamic handler - so it accepts data
+////    @PostMapping("hello")
+//    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")//putting this above at the top, you can just see that it lives at /hello/hello
+//    @ResponseBody
+//    public String helloWithQueryParam(@RequestParam String name) {
+//        return "Hello, " + name + "!";
+//    }
+//
+//    //Handles requests of the form /hello/Launchcode
+//    @GetMapping("hello/{name}")
+//    @ResponseBody
+//    public String helloWithPathParam(@PathVariable String name) {
+//        return "hello, " + name + "!";
+//    }
+//
+//
+//    @GetMapping("form")
+//   // @ResponseBody - this is removed when you return a template
+//    public String helloForm(){//here now you just return the name of the template
+//            return "form";
+//    }
+//}
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Created by Chris Bay
+ */
 @Controller
-@ResponseBody//You can put this up here and delete all the others below
-@RequestMapping("hello")//you can also put this up here - and it affects the route below, so
 public class HelloController {
-    //Handles request at path localhost:8080/hello
-//    @GetMapping("hello")//NOTE we use @PostMapping to do post requests
-//    @ResponseBody
-//    public String hello() {
-//        return "Hello, Spring!";
-//    }
-    //this handles request at /goodbye returns a static response
-    @GetMapping("goodbye")//putting this above at the top, you can just see that it lives at /hello/goodbye
+
+    @GetMapping("goodbye")
     @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
-    //create a handler that handles requests of /hello?name=LaunchCode you have to type localhost:8080/hello?name=name
-//    @GetMapping("hello")//dynamic handler - so it accepts data
-//    @PostMapping("hello")
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")//putting this above at the top, you can just see that it lives at /hello/hello
+    // Handles requests of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
     @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
-    //Handles requests of the form /hello/Launchcode
+    // Handles requests of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
     @ResponseBody
     public String helloWithPathParam(@PathVariable String name) {
-        return "hello, " + name + "!";
+        return "Hello, " + name + "!";
     }
 
-
+    // /hello/form
     @GetMapping("form")
-    @ResponseBody
-    public String helloForm(){
-        return "<html>" +
-                "<body>" +
-                "<form action='hello' method='post'>" + //submit a request to /hello
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+    public String helloForm() {
+        return "form";
     }
+
 }
